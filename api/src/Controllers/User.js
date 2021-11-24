359cade999a769c24520ee67c9ebe717348e2b01
pivotsk8 <the_pivotsk8@hotmail.com>
2021-11-23 21:18:54 -0500

    23/11/21 23h18 comienzo postuser

--- api/src/Controllers/User.js
+++ api/src/Controllers/User.js
@@ -2,29 +2,40 @@ const { User } = require('../db')
 const { v4: uuidv4 } = require('uuid')
 
 const postUser = async (req, res) => {
-  const { 
-      name, 
-      lastname,
-      email,
-      telephone,
-      facebook,
-      instagram,
-      password,
-      province, 
-      city,
-      street,
-      patent,
-      dni,  
-      age,
-      about,
-      genre,  
-      id=uuidv4(),
+    const {
+        name,
+        lastname,
+        email,
+        telephone,
+        facebook,
+        instagram,
+        password,
+        province,
+        city,
+        street,
+        patent,
+        dni,
+        age,
+        about,
+        genre,
     } = req.body
-  const user = await User.findOrCreate({
-    id: uuidv4(),
-    name,
-    email,
-    password
-  })
-  res.send(user)
+    const user = await User.findOrCreate({
+        id: User.id,
+        name: name,
+        lastname: lastname,
+        email: email,
+        telephone: telephone,
+        facebook: facebook,
+        instagram: instagram,
+        password: password,
+        province: province,
+        city: city,
+        street: street,
+        patent: city,
+        dni: dni,
+        age: age,
+        about: about,
+        genre: genre,
+    })
+    res.send(user)
 }
