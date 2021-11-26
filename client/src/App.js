e1ba2ebd615d6d79ba19b4dba7a2278ca02fa545
Mateo Filip <mateofernandofilip@gmail.com>
2021-11-25 22:31:39 -0300

    Avances hechos en CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import LandingPage from "./components/LandingPage"
-import Home from "./components/Home"
-import CreateRoute from "./components/CreateRoute"
-import UserDetails from "./components/UserDetails"
-import { useAuth0 } from "@auth0/auth0-react";
-import { LoginButton } from "./components/Login";
-import { LogoutButton } from "./components/Logout";
-import { Profile } from "./components/Profile";
+import './Sass/Styles/App.scss';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import Home from './components/Home';
+import CreateRoute from './components/CreateRoute';
+import UserDetails from './components/UserDetails';
+import { useAuth0 } from '@auth0/auth0-react';
+import { LoginButton } from './components/Login';
+import { LogoutButton } from './components/Logout';
+import { Profile } from './components/Profile';
 import Registro from './components/Registro';
 
 function App() {
@@ -15,8 +15,8 @@ function App() {
 
   return (
     <BrowserRouter>
-    <div className="App">
-    {isAuthenticated ? (
+      <div className="App">
+        {isAuthenticated ? (
           <>
             <Profile />
             <LogoutButton />
@@ -24,14 +24,16 @@ function App() {
         ) : (
           <LoginButton />
         )}
-     { <Switch>                                               
-    <Route exact path= "/" component= { LandingPage }/>
-    <Route path= "/home" component= { Home }/>
-    <Route path="/route" component= { CreateRoute }/>
-    <Route path='/register' component = { Registro }/>
-    <Route path="/user/:id" component= { UserDetails }/>
-      </Switch>}
-    </div>
+        {
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/home" component={Home} />
+            <Route path="/route" component={CreateRoute} />
+            <Route path="/register" component={Registro} />
+            <Route path="/user/:id" component={UserDetails} />
+          </Switch>
+        }
+      </div>
     </BrowserRouter>
   );
 }
--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,25 +1,18 @@
-import React from "react";
-import { NavLink } from 'react-router-dom'; 
-import styles from "../styles/LandingPage.module.css";
-
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../Sass/Styles/LandingPage.scss';
 
 export default function LandingPage() {
-    return (
-      <div className={styles.landingpage}>
-          <div className={styles.bkg}>
-         <div className={styles.containerAll}>
-         <div className={styles.container}>
-             <div className={styles.centrador}>
-            <h1  className={styles.title}> Subí que te Llevo! &#128662;</h1>
-            <div className={styles.buttonPosition}>
-            </div>
-            <NavLink to ="/home">
-                <button className={styles.button}>Ingresar</button>
-            </NavLink>
-            </div>
-            </div>
-            </div>
-        </div>
-        </div> 
-    )
-} 
+  return (
+    <div className="LandingPage">
+      <div className="LandingPageContainer">
+        <h1 className="title">subiquetellevo</h1>
+        <p className="paragraph">Una forma distinta de viajar.</p>
+        <div></div>
+        <NavLink to="/home">
+          <button className="button">Ingresar</button>
+        </NavLink>
+      </div>
+    </div>
+  );
+}
