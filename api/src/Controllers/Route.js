bdbb7dc6408fa70b80a3825bce857bdc03db470e
Juan Moldes <juanignaciomoldes1@gmail.com>
2021-11-30 17:33:11 -0300

    agrego los filtros para rutas por nombre de origen, destino y cantidad de lugares

--- api/src/Controllers/Route.js
+++ api/src/Controllers/Route.js
@@ -73,7 +73,7 @@ const postRoute = async (req, res, next) => {
       points
     } = req.body;
 
-    let price = km * 7 + km * 10/100;
+    let price = km * 7 + (km * 7 * (10/100));
 
     const route = await Route.create(
       {
@@ -104,7 +104,7 @@ const postRoute = async (req, res, next) => {
 
 const getRoute = async (req, res, next) => {
   try {
-    let { restriction,price,time,date} = req.query;
+    let {restriction,price,time,date,from,to,place} = req.query;
     const { id } = req.params;
     let routes;
 
@@ -132,6 +132,20 @@ const getRoute = async (req, res, next) => {
 
     });
 
+    if (from) {
+      routes = routes.filter((route) => {
+        if (route.originName === from) return true;
+        else return false;
+      });
+    }
+
+    if (to) {
+      routes = routes.filter((route) => {
+        if (route.destinyName === to) return true;
+        else return false;
+      });
+    }
+
     if (restriction) {
       restriction = restriction.split(",");
 
@@ -151,6 +165,12 @@ const getRoute = async (req, res, next) => {
       });
     }
 
+    if (place) {
+      routes = routes.filter((route) => {
+        if (route.place >= place) return true;
+        else return false;
+      })
+    }
 
     if (price === "desc" || !price || price === "") {
       routes = routes.sort((a, b) => b.price - a.price);
