dc36ea61e8e9db33e06d9a3df4b3589df1d39249
Nelson Albera <nalbera@hotmail.com>
2021-12-02 17:14:41 -0300

    modelos actualizados

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,9 +3,7 @@ const mapsRoute = require('./Maps');
 const userRoute = require('./User');
 const carRoute = require('./Car');
 const suggestionRoute = require('./SuggestionBox');
-const setDbRoute = require('./SetDb');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
+
 const router = Router();
 
 // Configurar los routers
@@ -14,6 +12,6 @@ router.use('/maps', mapsRoute)
 router.use('/user', userRoute)
 router.use('/car', carRoute)
 router.use('/suggestionbox', suggestionRoute)
-router.use('/setdb', setDbRoute)
+
 
 module.exports = router;
