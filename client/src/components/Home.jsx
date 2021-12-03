3bc4d43579b9ba516a1fc81f690e06dfdd2ba4e7
Mateo Filip <mateofernandofilip@gmail.com>
2021-12-03 18:56:49 -0300

    Tailwind instalado, terminados componentes LandingPage y Home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,52 +1,54 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import "../Sass/Styles/Home.scss";
-import header from "../img/header.png";
-import group2 from "../img/group2.png";
-import SearchBarHome from "./SearchBarHome";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Sass/Styles/Home.scss';
+import header from '../img/header.png';
+import group2 from '../img/group2.png';
+import SearchBarHome from './SearchBarHome';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
-  const  users = useSelector(state => state.user);
+  const users = useSelector((state) => state.user);
   return (
-    <div className="Home">
+    <div className="w-screen">
       {/* <div>
         <img className="fotoHeader" src={header} alt="header" />
       </div> */}
-      <div>
-        <SearchBarHome />
-      </div>
-      <div className="Article">
-        <div></div>
-        <div>
-          <img className="foto" src={group2} alt="Home" />
-        </div>
-        <article>
-          <h1>Save money while driving</h1>
-          <p> Publish your next round trip on GiveMeaRide and get, on average, 80 dollars * 
-            of your passengers. You will only need a couple of minutes to publish your route. 
-            Do we share a trip?
-
-          <p>* Average amount received by drivers in 2021.</p>
+      <div>{/* <SearchBarHome /> */}</div>
+      <div className="p-5 md:p-20">
+        <img
+          className="w-full mb-5 rounded-xl shadow-xl lg:float-left lg:w-6/12 "
+          src={group2}
+          alt="Home"
+        />
+        <article className="lg:float-right lg:w-6/12 lg:text-left lg:p-8">
+          <h1 className="m-5 font-semibold text-2xl">
+            Save money while driving!
+          </h1>
+          <p className="m-5 font-semibold">
+            Publish your next trip on GimmeARide and get, on average, 80
+            dollars* from your passengers. You'll only need a couple of minutes
+            to publish your trip. Wanna share it?
+            <p className="mt-5 font-light">
+              * Average amount received by drivers in 2021.
+            </p>
           </p>
-          <div className="searchContainer">
           {
-            <NavLink
-              className="searchContainerItem"
+            <Link
               to={
                 !users.dni
-                  ? "/register"
+                  ? '/register'
                   : users.name && users.cars.length === 0
-                  ? "/car"
+                  ? '/car'
                   : users.name && users.cars[0].patent
-                  ? "/route"
-                  : ""
+                  ? '/route'
+                  : ''
               }
             >
-              <button className="button">Post a Trip</button>
-            </NavLink>
+              <button className="font-semibold bg-green-500 m-2 py-2 px-6 md:py-4 md:px-12 border-0 rounded-3xl text-white transition duration-500 ease-in-out hover:bg-green-700">
+                Post a Trip
+              </button>
+            </Link>
           }
-          </div>
         </article>
       </div>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const auth0Client = process.env.REACT_APP_AUTH0_CLIENT;
@@ -18,13 +18,13 @@ ReactDOM.render(
         domain="dev-ldy8yn3a.us.auth0.com"
         clientId="UeNorZxq5mET0n5RaFSKS4E6Y6SCm2i6"
         // redirectUri={window.location.origin} >
-        redirectUri={"http://localhost:3000/register"}
+        redirectUri={'http://localhost:3000/register'}
       >
         <App />
       </Auth0Provider>
     </React.StrictMode>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
