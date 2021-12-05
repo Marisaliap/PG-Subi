bd0aa229c3d7648db3b8abc5509e34353999f17b
Juan Moldes <juanignaciomoldes1@gmail.com>
2021-12-05 19:36:15 -0300

    Agrego estilos al routeDetail, cardUser,carRoute, NavBarFilter y .ceil

--- client/src/components/RouteDetails.jsx
+++ client/src/components/RouteDetails.jsx
@@ -21,31 +21,35 @@ const RouteDetails = () => {
       <NavBarFilter />
 
       <div className="RouteCardContainer">
-        {getRoutes.map((route, i) => (
-          <Link to={`/route/${route.id}`} style={{ textDecoration: 'none' }}>
-            <div className="RouteCard">
-
-             { route.users && <CardUser
-                photo={ route.users.length > 0 && route.users[0].photo}
-                name={route.users.length > 0 && route.users[0].name}
-                calification={route.users.length > 0 && route.users[0].calification}
-                key={i}
-              />}
-
-
-              <CardRoute
-                origin={route.originName}
-                destiny={route.destinyName}
-                infoRoute={route.infoRoute}
-                date={route.date}
-                hours={route.hours}
-                place={route.place}
-                key={i + 1}
-                price={route.price}
-              />
-            </div>
-          </Link>
-        ))}
+        {
+          getRoutes.map((route, i) => (
+            <Link className="link" to={`/route/${route.id}`} >
+              <div className="RouteCard">
+               {
+                 route.users && <CardUser
+                  photo={ route.users.length > 0 && route.users[0].photo}
+                  name={route.users.length > 0 && route.users[0].name}
+                  lastName={route.users.length > 0 && route.users[0].lastName}
+                  genre={route.users.length > 0 && route.users[0].genre}
+                  age={route.users.length > 0 && route.users[0].age}
+                  calification={route.users.length > 0 && route.users[0].calification}
+                  key={i}
+                />
+              }
+                <CardRoute
+                  origin={route.originName}
+                  destiny={route.destinyName}
+                  infoRoute={route.infoRoute}
+                  date={route.date}
+                  hours={route.hours}
+                  place={route.place}
+                  key={i + 1}
+                  price={route.price}
+                />
+              </div>
+            </Link>
+          ))
+        }
       </div>
     </div>
   );
