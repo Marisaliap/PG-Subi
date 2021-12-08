691019a0d939aca3a2b9c42ff50aec36752a5a98
fabsantandrea <fabsantandrea@gmail.com>
2021-12-07 21:32:45 -0300

    Agregué validaciones al searchbarHome y unas cositas mas ;)

--- client/src/components/SearchBarPostRuta.jsx
+++ client/src/components/SearchBarPostRuta.jsx
@@ -132,7 +132,7 @@ export default function SearchBar() {
         </FormattedMessage>
 
         <datalist id="cities">
-          {cities && cities.map((city) => <option>{city.name}</option>)}
+          {cities && cities.map((city) => city.name !== inputs.Origin && <option>{city.name}</option>)}
         </datalist>
         <p>{validations && validations.Origin}</p>
 
@@ -149,7 +149,7 @@ export default function SearchBar() {
         </FormattedMessage>
 
         <datalist id="cities2">
-          {cities2 && cities2.map((city) => <option>{city.name}</option>)}
+          {cities2 && cities2.map((city) => city.name !== inputs.Destination && <option>{city.name}</option>)}
         </datalist>
         <p>{validations && validations.Destination}</p>
         <div>
