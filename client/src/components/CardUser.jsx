d44d20f766b3e7df72a203b59afc7a7f2617796f
Marisa Lia Pascal <87451265+Marisaliap@users.noreply.github.com>
2021-12-12 23:08:25 -0300

    Revert "Merge branch 'mateo' into MarisaLiap"
    This reverts commit 4a18741deaff38a7e137b9be6056984548c0a40f, reversing
    changes made to 919770746f2e5e32c9b8b3e0dca65b8cb37df45f.

--- client/src/components/CardUser.jsx
+++ client/src/components/CardUser.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
-import '../Sass/Styles/CardUser.scss';
-import { BsGenderFemale, BsGenderMale } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { getUserDetail } from '../actions';
-import RatingStar from './RatingStar';
+import React, { useEffect } from "react";
+import "../Sass/Styles/CardUser.scss";
+import { BsGenderFemale, BsGenderMale } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getUserDetail } from "../actions";
+import RatingStar from "./RatingStar";
 
 export default function CardUser({
   photo,
@@ -16,27 +16,36 @@ export default function CardUser({
   email,
   ...props
 }) {
+
   const dispatch = useDispatch();
-  useEffect(() => dispatch(getUserDetail(email)), [dispatch]);
+  useEffect(() => dispatch(getUserDetail(email)), [dispatch])
   function genderIcon(gender) {
-    if (gender === 'Male') {
+   
+
+    if (gender === "Male") {
       return <BsGenderMale className="maleGender" />;
-    } else if (gender === 'Female') {
+    } else if (gender === "Female") {
       return <BsGenderFemale className="femaleGender" />;
     }
   }
 
   return (
-    <div className="CardUser" {...props}>
-      <img src={photo} alt="" />
-      <h5>
-        {name} {lastName}
-      </h5>
-      {/* <h5>{age}</h5> */}
-      {/* <h5>{genderIcon(genre)}</h5> */}
-      <h5>
-        <RatingStar Rating={calification} />
-      </h5>
-    </div>
+    <>
+      <div className="CardUser" {...props}>
+      
+          <img src={photo} alt="" />
+          <h5>
+            {name} {lastName}
+          </h5>
+          <h5>{age}</h5>
+          <h5>{genderIcon(genre)}</h5>
+          <h5>
+            <RatingStar
+            Rating={calification}
+            />
+          </h5>
+      
+      </div>
+    </>
   );
 }
--- client/src/components/RouteDetails.jsx
+++ client/src/components/RouteDetails.jsx
@@ -1,45 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { allRoutes, deleteRouteFromDb } from '../actions';
-import CardRoute from './CardRoute';
-import CardUser from './CardUser';
-import NavBarFilter from './NavBarFilter';
-import '../Sass/Styles/RouteDetails.scss';
-import Pagination from './Pagination';
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { allRoutes, deleteRouteFromDb } from "../actions";
+import CardRoute from "./CardRoute";
+import CardUser from "./CardUser";
+import NavBarFilter from "./NavBarFilter";
+import "../Sass/Styles/RouteCardContainer.scss";
+import "../Sass/Styles/RouteCard.scss";
+import Pagination from "./Pagination" ;
 // import {CardCar} from "./CardCar";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const RouteDetails = ({ match }) => {
+const RouteDetails = ({match}) => {
+
   const dispatch = useDispatch();
 
   const { getRoutes } = useSelector((state) => state);
-  const { filteredRouteFromDb } = useSelector((state) => state);
+  const { filteredRouteFromDb } = useSelector(state => state)
 
-  useEffect(() => {
-    dispatch(allRoutes());
-  }, []);
+  useEffect(() => {dispatch(allRoutes())}, []);
   useEffect(() => {
     return () => {
-      dispatch(deleteRouteFromDb('lala'));
+     dispatch(deleteRouteFromDb('lala')) 
     };
   }, []);
 
   // ------------------<paged>------------------
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [routesPerPage, setRoutesPerPage] = useState(6);
-  const indexOfLastRoute = currentPage * routesPerPage;
-  const indexOffirstRoute = indexOfLastRoute - routesPerPage;
-  const currentRoutes =
-    filteredRouteFromDb.length > 0 || match.params.id
-      ? filteredRouteFromDb.slice(indexOffirstRoute, indexOfLastRoute)
-      : getRoutes.slice(indexOffirstRoute, indexOfLastRoute);
+  const [currentPage, setCurrentPage] = useState(1)      
+  const [ routesPerPage, setRoutesPerPage ] = useState (6)                   
+  const indexOfLastRoute = currentPage * routesPerPage      
+  const indexOffirstRoute = indexOfLastRoute - routesPerPage   
+  const currentRoutes = ( filteredRouteFromDb.length > 0 || match.params.id ) ? (
+    filteredRouteFromDb.slice(indexOffirstRoute, indexOfLastRoute)
+     )  :( 
+       getRoutes.slice(indexOffirstRoute, indexOfLastRoute) 
+       )   
 
-  const pagedTotal = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
+  const pagedTotal = (pageNumber) => {                      
+    setCurrentPage(pageNumber)                        
+}                                                    
 
-  // ------------------<pagedEnd>------------------
+// ------------------<pagedEnd>------------------
 
   return (
     <div className="RouteDetails">
@@ -49,26 +50,21 @@ const RouteDetails = ({ match }) => {
         {currentRoutes.map((route, i) => (
           <Link className="link" id="link" to={`/route/${route.id}`}>
             <div className="RouteCard">
-              {console.log(route.users)}
-              {route.users && (
+              {console.log(route.users)}{route.users && (
                 <CardUser
                   photo={route.users.length > 0 && route.users[0].photo}
                   name={route.users.length > 0 && route.users[0].name}
                   lastName={route.users.length > 0 && route.users[0].lastName}
                   genre={route.users.length > 0 && route.users[0].genre}
                   age={route.users.length > 0 && route.users[0].age}
-                  email={
-                    route.users.length > 0 &&
-                    route.users[0].UserRoutes.userEmail
-                  }
+                  email={route.users.length > 0 && route.users[0].UserRoutes.userEmail}
                   calification={
                     route.users.length > 0 && route.users[0].calification
                   }
                   key={i}
                 />
+              
               )}
-              <hr />
-
               <hr />
 
               <CardRoute
@@ -87,9 +83,9 @@ const RouteDetails = ({ match }) => {
       </div>
       <div>
         <Pagination
-          routesPerPage={routesPerPage}
-          getRoutes={getRoutes.length}
-          pagedTotal={pagedTotal}
+          routesPerPage= { routesPerPage }
+          getRoutes= { getRoutes.length }                    
+          pagedTotal= { pagedTotal }
         />
       </div>
     </div>
