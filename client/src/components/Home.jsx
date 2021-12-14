c9f5a531afbeab687b525e26ae354b276f601fcc
Mateo Filip <mateofernandofilip@gmail.com>
2021-12-14 20:23:34 -0300

    Homepage corregido y responsive

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import "../Sass/Styles/Home.scss";
-import navigator from "../img/navigator.svg";
-import cityDriver from "../img/cityDriver.svg";
-import carDriver from "../img/carDriver.svg";
-import SearchBarHome from "./SearchBarHome";
-import { useSelector, useDispatch } from "react-redux";
-import { FormattedMessage } from "react-intl";
-import { getUserProfile, getAllUsers, deleteRouteFromDb } from "../actions";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Link } from "react-router-dom";
-import Swal from "sweetalert2";
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import '../Sass/Styles/Home.scss';
+import navigator from '../img/navigator.svg';
+import cityDriver from '../img/cityDriver.svg';
+import carDriver from '../img/carDriver.svg';
+import SearchBarHome from './SearchBarHome';
+import { useSelector, useDispatch } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import { getUserProfile, getAllUsers, deleteRouteFromDb } from '../actions';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 export default function Home() {
   const { isAuthenticated } = useAuth0();
@@ -26,7 +26,7 @@ export default function Home() {
   useEffect(() => {
     window.scrollTo({
       top: 0,
-      behavior: "instant",
+      behavior: 'instant',
     });
     dispatch(getAllUsers());
   }, []);
@@ -34,10 +34,10 @@ export default function Home() {
   function handleClick() {
     if (!isAuthenticated) {
       return new Swal({
-        icon: "warning",
-        title: "Sorry",
-        text: "You need to be logged in to post a trip!",
-        confirmButtonText: "Alright",
+        icon: 'warning',
+        title: 'Sorry',
+        text: 'You need to be logged in to post a trip!',
+        confirmButtonText: 'Alright',
       });
     }
   }
@@ -53,72 +53,70 @@ export default function Home() {
         <SearchBarHome />
       </section>
 
-      <section className="lowerSection">
-          <div className="centrateee">
-            <img className="homepageImage" src={navigator} alt="Home" />
-          </div>
-          <div className="centrateee">
-            <article>
-              <h1>
-                <FormattedMessage
-                  id="home.title1"
-                  defaultMessage="Save money while driving"
-                />
-              </h1>
+      <section className="upperSection">
+        <img className="homepageImage" src={navigator} alt="Home" />
+        <article>
+          <h1>
+            <FormattedMessage
+              id="home.title1"
+              defaultMessage="Save money while driving"
+            />
+          </h1>
 
-              <p className="description">
-                <FormattedMessage
-                  id="home.p1"
-                  defaultMessage="Publish your next round trip on Gimme a Ride and get, on average, 80
+          <p className="description">
+            <FormattedMessage
+              id="home.p1"
+              defaultMessage="Publish your next round trip on Gimme a Ride and get, on average, 80
                 dollars * of your passengers. You will only need a couple of minutes
                 to publish your route. Do we share a trip?"
-                />
-              </p>
-              <p className="detail">
-                <FormattedMessage
-                  id="home.p2"
-                  defaultMessage="* Average amount received by drivers in 2021."
-                />
-              </p>
+            />
+          </p>
+          <p className="detail">
+            <FormattedMessage
+              id="home.p2"
+              defaultMessage="* Average amount received by drivers in 2021."
+            />
+          </p>
 
-              <div>
-                {isAuthenticated ? (
-                  <button
-                    className="button"
-                    onClick={() => {
-                      dispatch(deleteRouteFromDb("lala"));
-                      history.push("/route-list");
-                    }}
-                  >
-                    <FormattedMessage
-                      id="home.routes"
-                      defaultMessage="See all routes available!"
-                    />
-                  </button>
-                ) : (
-                  <button onClick={handleClick} className="button">
-                    <FormattedMessage
-                      id="home.routes"
-                      defaultMessage="See all routes available!"
-                    />
-                  </button>
-                )}
-              </div>
-            </article>
-          </div>
+          {isAuthenticated ? (
+            <button
+              className="button"
+              onClick={() => {
+                dispatch(deleteRouteFromDb('lala'));
+                history.push('/route-list');
+              }}
+            >
+              <FormattedMessage
+                id="home.routes"
+                defaultMessage="See all routes available!"
+              />
+            </button>
+          ) : (
+            <button onClick={handleClick} className="button">
+              <FormattedMessage
+                id="home.routes"
+                defaultMessage="See all routes available!"
+              />
+            </button>
+          )}
+        </article>
+      </section>
 
-            <div className="centrateee">
-              <img className="SVGs" src={cityDriver} alt="Are you a passenger?" />
-              <p>
-                <FormattedMessage id="home.imagenAbajo1" defaultMessage="" />
-              </p>
-            </div>
-            <div className="centrateee">
-              <img className="SVGs" src={carDriver} alt="Are you a passenger?" />
-              <p>
-                <FormattedMessage id="home.imagenAbajo2" defaultMessage="" />
-              </p>
-            </div>
+      <section className="lowerSection">
+        <div>
+          <img className="SVGs" src={cityDriver} alt="Are you a passenger?" />
+          <h3>Are you a passenger?</h3>
+          <p>
+            <FormattedMessage id="home.imagenAbajo1" defaultMessage="" />
+          </p>
+        </div>
+        <div>
+          <img className="SVGs" src={carDriver} alt="Are you a driver?" />
+          <h3>Are you a driver?</h3>
+          <p>
+            <FormattedMessage id="home.imagenAbajo2" defaultMessage="" />
+          </p>
+        </div>
       </section>
     </div>
   );
