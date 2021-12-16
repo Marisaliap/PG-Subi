235b76000a3eaeaa35a8d53c8bc93db72831ce7b
fabsantandrea <fabsantandrea@gmail.com>
2021-12-15 22:27:01 -0300

    se terminó de arreglar MP

--- api/src/Controllers/MercadoPago.js
+++ api/src/Controllers/MercadoPago.js
@@ -1,69 +1,64 @@
-const mercadopago = require ('mercadopago');
-const { Order, Route, User } = require('../db.js');
+const mercadopago = require("mercadopago");
+const { Order, Route, User } = require("../db.js");
 
-const axios = require('axios')
+const axios = require("axios");
 const { ACCESS_TOKEN } = process.env;
 
-const postMP = async (req,res,next) => {
+const postMP = async (req, res, next) => {
   try {
-    let {
-      idUser,
-      idRoute,
-      title,
-      price,
-    } = req.body
-    
+    let { idUser, idRoute, title, price } = req.body;
+
     price = parseInt(price);
-      const orden = await Order.create({status:'processing'});
-      const route = await Route.findByPk(idRoute);
-      await route.addOrder(orden.id);
-      const user = await User.findByPk(idUser);
-      await user.addOrder(orden.id);
+    const orden = await Order.create({ status: "processing" });
+    const route = await Route.findByPk(idRoute);
+    await route.addOrder(orden.id);
+    const user = await User.findByPk(idUser);
+    await user.addOrder(orden.id);
 
-    mercadopago.configure({access_token: ACCESS_TOKEN});
+    mercadopago.configure({ access_token: ACCESS_TOKEN });
 
-
-    const item_ml =
-      {
-        title: title,
-        unit_price: price,
-        quantity: 1,
-      };
-
+    const item_ml = {
+      title: title,
+      unit_price: price,
+      quantity: 1,
+    };
 
     let preference = {
       items: [item_ml],
-      external_reference : `${orden.id}`,
+      external_reference: `${orden.id}`,
       payment_methods: {
         excluded_payment_types: [
           {
-            id: "ticket"
-          }
+            id: "ticket",
+          },
         ],
-        installments: 1
-  	  },
-      auto_return: 'approved',
+        installments: 1,
+      },
+      auto_return: "approved",
       back_urls: {
-        success: 'http://localhost:3001/mercadopago/payment?idRoute=' + idRoute + '&idUser=' + idUser,
-        failure: 'http://localhost:3000/route/' + idRoute
-      }
+        success:
+          "https://pg-subi-deployed.herokuapp.com/mercadopago/payment?idRoute=" +
+          idRoute +
+          "&idUser=" +
+          idUser,
+        failure: "https://gimmearide.vercel.app/route/" + idRoute,
+      },
     };
 
-    const response = await mercadopago.preferences.create(preference)
+    const response = await mercadopago.preferences.create(preference);
 
     global.id = response.body.id;
 
-    res.json({id:orden.id,init_point: response.body.sandbox_init_point});
-
-  } catch(err)  {
-    next(err)
+    res.json({ id: orden.id, init_point: response.body.sandbox_init_point });
+  } catch (err) {
+    next(err);
   }
-}
+};
 
-
-const getMPPayment = async (req,res) => {
-  const { payment_id,payment_status,external_reference,merchant_order_id } =  req.query;
-  const {idRoute, idUser} = req.query
+const getMPPayment = async (req, res) => {
+  const { payment_id, payment_status, external_reference, merchant_order_id } =
+    req.query;
+  const { idRoute, idUser } = req.query;
 
   // Route.findByPk(idRoute)
   // .then((route) => {
@@ -72,32 +67,36 @@ const getMPPayment = async (req,res) => {
   //     idUser: idUser
   //   })
   // })
- const route = await Route.findByPk(idRoute)
+  const route = await Route.findByPk(idRoute);
 
- const editRoute = await axios.put('http://localhost:3001/maps/route/' + route.id, {
-  place: route.place - 1,
-  idUser: idUser
-})
+  const editRoute = await axios.put(
+    "http://localhost:3001/maps/route/" + route.id,
+    {
+      place: route.place - 1,
+      idUser: idUser,
+    }
+  );
 
   Order.findByPk(external_reference)
-  .then((order) => {
-    // console.log(external_reference, 'soy reference')
-    order.payment_id= payment_id
-    order.payment_status= payment_status
-    order.merchant_order_id = merchant_order_id
-    order.status = "created"
-    order.price = route.price
-    order.save()
-    .then(() => res.redirect("http://localhost:3000/home"))
-    .catch((err) => res.redirect(`http://localhost:3000/?error=${err}&where=al+salvar`))
-  })
+    .then((order) => {
+      // console.log(external_reference, 'soy reference')
+      order.payment_id = payment_id;
+      order.payment_status = payment_status;
+      order.merchant_order_id = merchant_order_id;
+      order.status = "created";
+      order.price = route.price;
+      order
+        .save()
+        .then(() => res.redirect("http://localhost:3000/home"))
+        .catch((err) =>
+          res.redirect(`http://localhost:3000/?error=${err}&where=al+salvar`)
+        );
+    })
 
-  .catch(err => res.redirect(`http://localhost:3000/?error=${err}&where=al+buscar`))
-}
-
-
-
-
+    .catch((err) =>
+      res.redirect(`http://localhost:3000/?error=${err}&where=al+buscar`)
+    );
+};
 
 // const refunded = async (req,res,next) => {
 //   const { idUser, idRoute } = req.query;
@@ -124,6 +123,4 @@ const getMPPayment = async (req,res) => {
 //     });
 // }
 
-
-
-module.exports = {postMP,getMPPayment,/*refunded*/}
+module.exports = { postMP, getMPPayment /*refunded*/ };
