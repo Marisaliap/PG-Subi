d8d5cfd8089ec65e0a1035c55254c2906617cf7c
pivotsk8 <the_pivotsk8@hotmail.com>
2022-01-18 14:29:20 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -108,7 +108,7 @@ export default function App() {
                       ) : (
                         ""
                       )}
-                    </Switch>
+                    </Switch>ñ
                   </>
                 ) : (
                   ""
